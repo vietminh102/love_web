7c25883dbad382a2598b63d5fe40789664c2bc02
Ta Minh Viet <tamuop102101@gmail.com>
2026-06-14 21:03:58 +0700

    fix limit size vite

--- lovelink-frontend/vite.config.ts
+++ lovelink-frontend/vite.config.ts
@@ -1,12 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin-allow-popups',
-    },
-  },
+  build: {
+    // 1. Tăng nhẹ giới hạn cảnh báo
+    chunkSizeWarningLimit: 1000, 
+    
+    // 2. Cắt nhỏ các thư viện nặng ra thành từng file riêng biệt
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // Tách thư viện hiệu ứng (Nặng nhất)
+            if (id.includes('framer-motion') || id.includes('motion')) {
+              return 'vendor-motion';
+            }
+            // Tách thư viện Icon
+            if (id.includes('lucide-react')) {
+              return 'vendor-lucide';
+            }
+            // Tách thư viện lõi của React
+            if (id.includes('react') || id.includes('react-dom') || id.includes('react-router')) {
+              return 'vendor-react';
+            }
+            // Gom các thư viện lặt vặt còn lại
+            return 'vendor-others';
+          }
+        }
+      }
+    }
+  }
 })
